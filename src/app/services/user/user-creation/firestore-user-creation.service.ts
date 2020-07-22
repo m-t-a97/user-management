06eb9ceeff6921e6a94a6cebe9723133cd321679
t.ahmed97 <t.ahmed97@outlook.com>
2020-07-22 01:31:41 +0100

    Added creation date to a user's document when they are created.

--- src/app/services/user/user-creation/firestore-user-creation.service.ts
+++ src/app/services/user/user-creation/firestore-user-creation.service.ts
@@ -3,6 +3,7 @@ import { UserCreationService } from './user-creation.service';
 import { IUser } from '../../../models/users/i-user';
 import { FirestoreService } from '../../firestore/firestore.service';
 import { DocumentReference } from '@angular/fire/firestore';
+import * as firebase from 'firebase';
 
 @Injectable()
 export class FirestoreUserCreationService implements UserCreationService {
@@ -15,6 +16,7 @@ export class FirestoreUserCreationService implements UserCreationService {
       .then((document: DocumentReference) => {
         const dataToAddToDocument = {
           uid: document.id,
+          created: firebase.firestore.FieldValue.serverTimestamp(),
         };
 
         document.set(dataToAddToDocument, { merge: true });
